e77ea16ef7c1fd31ea167773da1a85246d4f4c66
danish-mehmood <rdm355190@gmail.com>
2024-09-20 08:05:55 +0500

    added search , js and css

---
+++ js/dummy.js
@@ -0,0 +1,15 @@
+gsap.to(".green", {
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: "none",
+});
+
+gsap.to(".purple", {
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: "bounce.out",
+});
---
+++ my-first-post/index.md
@@ -0,0 +1,10 @@
+# My First Post
+
+
+<!--more-->
+
+## this is new
+
+<div class="surround"><div class="box green">linear</div>
+<div class="box purple">bounce</div></div>
+
